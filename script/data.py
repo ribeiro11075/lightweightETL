a26eb9c0a6d083c42861d735f0248f091b510468
ribeiro11075 <ribeiro11075@gmail.com>
2020-10-03 12:24:13 -0400

    Add more documentation and reduce code complexity

--- script/data.py
+++ script/data.py
@@ -12,9 +12,6 @@ import previousInterface as pi
 import logInterface as li
 
 # define variables
-# create interface objects
-# create configuration objects
-# create directory objects
 fileName = os.path.basename(__file__).split('.')[0]
 configurationInterface = ci.CONFIGURATION()
 databaseConfiguration = configurationInterface.getDatabaseConfiguration()
@@ -28,6 +25,7 @@ logInterface = li.LOG(logDirectory=logDirectory)
 def worker(readyQueue, completedQueue, activeJobs):
 
 	while True:
+
 		# get job from ready queue
 		job = readyQueue.get()
 		logInterface.logging.info('Starting {}'.format(job))
@@ -45,70 +43,53 @@ def worker(readyQueue, completedQueue, activeJobs):
 			preTargetAdhocQueries = [] if activeJobs[job]['preTargetAdhocQueries'] is None else [i for i in activeJobs[job]['preTargetAdhocQueries'] if i is not None]
 			postTargetAdhocQueries = [] if activeJobs[job]['postTargetAdhocQueries'] is None else [i for i in activeJobs[job]['postTargetAdhocQueries'] if i is not None]
 			sourceQuery = activeJobs[job]['sourceQuery']
-
 			sourceDatabaseConnectionSettings = databaseConfiguration[sourceDatabase]
 			targetDatabaseConnectionSettings = databaseConfiguration[targetDatabase]
 
-			# create database interface
 			# get data from source
-			# close database connection
 			databaseInterface = di.DATABASE(connectionSettings=sourceDatabaseConnectionSettings)
 			data = databaseInterface.query(query=sourceQuery)
 			databaseInterface.close()
 
-			# create database interface
 			# get column names
-			# close database connection
 			databaseInterface = di.DATABASE(connectionSettings=targetDatabaseConnectionSettings)
 			columns = databaseInterface.getAllColumnNames(table=targetaTableFinal)
 			databaseInterface.close()
 
-			# create transform Interface
 			# transform data
 			transformInterface = ti.TRANSFORM(data=data, columns=columns, columnTransforms=columnTransforms)
 			data = transformInterface.transform()
 
-			# create database interface
 			# truncate staging table
 			# insert data into staging table
-			# close database connection
 			databaseInterface = di.DATABASE(connectionSettings=targetDatabaseConnectionSettings)
 			databaseInterface.truncate(table=targetTableStage) if targetTableStage else None
 			databaseInterface.insert(table=targetTableStage, data=data, chunkSize=chunkSize) if targetTableStage else None
 			databaseInterface.close()
 
 			# iterate through pre adhoc queries to be run
-			# create database interface
 			# run adhoc queries
-			# close database connection
 			for preTargetAdhocQuery in preTargetAdhocQueries:
 				databaseInterface = di.DATABASE(connectionSettings=targetDatabaseConnectionSettings)
 				databaseInterface.alter(preTargetAdhocQuery)
 				databaseInterface.close()
 
 			# check if target table should be entirely replaced
-			# create database interface
 			# swap staging table with target table
-			# close database connection
 			if insertStrategy == 'swap':
 				databaseInterface = di.DATABASE(connectionSettings=targetDatabaseConnectionSettings)
-				print(targetTableStage)
 				databaseInterface.swap(targetTable=targetaTableFinal, stageTable=targetTableStage)
 				databaseInterface.close()
 
 			# check if target table should be appended to
-			# create database interface
 			# append data from stagin table to target table
-			# close database connection
 			if insertStrategy == 'upsert':
 				databaseInterface = di.DATABASE(connectionSettings=targetDatabaseConnectionSettings)
 				databaseInterface.upsert(table=targetaTableFinal, data=data, chunkSize=chunkSize) if not targetTableStage else databaseInterface.upsertFromStage(targetTable=targetaTableFinal, stageTable=targetTableStage)
 				databaseInterface.close()
 
 			# iterate through post adhoc queries to be run
-			# create database interface
 			# run adhoc queries
-			# close database connection
 			for postTargetAdhocQuery in postTargetAdhocQueries:
 				databaseInterface = di.DATABASE(connectionSettings=targetDatabaseConnectionSettings)
 				databaseInterface.alter(postTargetAdhocQuery)
@@ -132,23 +113,21 @@ if __name__ == '__main__':
 	logInterface.logging.info('Initiated script/data.py')
 
 	while True:
+		
 		databaseConfiguration = configurationInterface.getDatabaseConfiguration()
 		jobConfiguration = configurationInterface.getJobConfiguration(job=fileName)
 		previousDirectory = configurationInterface.getHistoryDirectory(historyType='previous', fileName=fileName)
 
-		# create previous interface
 		# get previous
 		previousInterface = pi.PREVIOUS(previousDirectory=previousDirectory)
 		previous = previousInterface.previous
 
 		# create object to manage processes
 		# create pool of processes
-		# run processes
 		workerInterface = wi.WORKER(jobs=jobConfiguration['jobs'], previous=previous)
 		pool = mp.Pool(jobConfiguration['workers'], worker, (workerInterface.readyQueue, workerInterface.completedQueue, workerInterface.activeJobs))
 		workerInterface.run()
 
-		# close pool
 		# sleep to ensure program does not run at 100% utilization
 		pool.close()
 		time.sleep(.5)
--- script/scramble.py
+++ script/scramble.py
@@ -7,25 +7,20 @@ import configurationInterface as ci
 import databaseInterface as di
 import scrambleInterface as si
 import workerInterface as wi
-import previousInterface as pi
 import logInterface as li
 
 # define variables
-# create interface objects
-# create configuration objects
-# create directory objects
 fileName = os.path.basename(__file__).split('.')[0]
 configurationInterface = ci.CONFIGURATION()
 databaseConfiguration = configurationInterface.getDatabaseConfiguration()
 jobConfiguration = configurationInterface.getJobConfiguration(job=fileName)
-previousDirectory = configurationInterface.getHistoryDirectory(historyType='previous', fileName=fileName)
-previousInterface = pi.PREVIOUS(previousDirectory=previousDirectory)
 logDirectory = configurationInterface.getHistoryDirectory(historyType='log', fileName=fileName)
 logInterface = li.LOG(logDirectory=logDirectory)
 
 def worker(readyQueue, completedQueue, activeJobs):
 
     while True:
+
         # get job from ready queue
         job = readyQueue.get()
         logInterface.logging.info('Starting {}'.format(job))
@@ -42,20 +37,19 @@ def worker(readyQueue, completedQueue, activeJobs):
             randomSalt = 	activeJobs[job]['randomSalt']
             preTargetAdhocQueries = [] if activeJobs[job]['preTargetAdhocQueries'] is None else [i for i in activeJobs[job]['preTargetAdhocQueries'] if i is not None]
             postTargetAdhocQueries = [] if activeJobs[job]['postTargetAdhocQueries'] is None else [i for i in activeJobs[job]['postTargetAdhocQueries'] if i is not None]
-
-            # define database interface connection configuration
-            # create database interface
             connectionSettings = databaseConfiguration[database]
-            databaseInterface = di.DATABASE(connectionSettings=connectionSettings)
 
-            # run prerequisite adhoc queries
-            for preTargetAdhocQuery in preTargetAdhocQueries:
-                databaseInterface.alter(query=preTargetAdhocQuery)
+			# iterate through pre adhoc queries to be run
+			# run adhoc queries
+			for preTargetAdhocQuery in preTargetAdhocQueries:
+				databaseInterface = di.DATABASE(connectionSettings=connectionSettings)
+				databaseInterface.alter(preTargetAdhocQuery)
+				databaseInterface.close()
 
             # define dynamic SQL to get all data
             # get data, column names, and column types
-            # close database connection
             dataQuery = "select * from {}".format(table)
+            databaseInterface = di.DATABASE(connectionSettings=connectionSettings)
             data = databaseInterface.query(query=dataQuery)
             columns = databaseInterface.getAllColumnNames(table=table)
             dataTypes = databaseInterface.getAllColumnTypes(table=table)
@@ -64,28 +58,27 @@ def worker(readyQueue, completedQueue, activeJobs):
             # check if any data exists
             if data:
 
-                # create scramble interface
                 scrambleInterface = si.SCRAMBLE(job=job, data=data, columns=columns, dataTypes=dataTypes, defaultColumnValues=defaultColumnValues,
                                                             identifierColumns=identifierColumns, scrambleColumns=scrambleColumns, randomColumns=randomColumns,
                                                             allDataRandom=allDataRandom, randomSalt=randomSalt)
 
                 # scramble data
                 # define database interface connection configuration
-                # create database interface
+                scrambleInterface.scramble()
+
 				# truncate tables
                 # insert data into target table
-                scrambleInterface.scramble()
-                connectionSettings = databaseConfiguration[database]
                 databaseInterface = di.DATABASE(connectionSettings=connectionSettings)
                 databaseInterface.truncate(table)
                 databaseInterface.insert(table=table, data=scrambleInterface.dataScrambled, chunkSize=5000)
-
-				# run post adhoc queries
-                for postTargetAdhocQuery in postTargetAdhocQueries:
-                    databaseInterface.alter(query=postTargetAdhocQuery)
-
-				# close database connection
                 databaseInterface.close()
+
+                # iterate through post adhoc queries to be run
+			    # run adhoc queries
+			    for postTargetAdhocQuery in postTargetAdhocQueries:
+				    databaseInterface = di.DATABASE(connectionSettings=connectionSettings)
+				    databaseInterface.alter(postTargetAdhocQuery)
+				    databaseInterface.close()
 
             status = True
             logInterface.logging.info('Completed {}'.format(job))
@@ -97,17 +90,17 @@ def worker(readyQueue, completedQueue, activeJobs):
 
         # place completed job in completed queue
         completedQueue.put({job: status})
-        previousInterface.addPrevious(job=job)
 
 
 if __name__ == '__main__':
 
+    logInterface.logging.info('Initiated script/scramble.py to scramble tables')
+
     # create object to manage processes
     # create pool of processes
-    # run processes
-    logInterface.logging.info('Initiated script/scramble.py to scramble tables')
-    previousInterface = pi.PREVIOUS(previousDirectory=previousDirectory)
     workerInterface = wi.WORKER(jobs=jobConfiguration['jobs'])
     pool = mp.Pool(jobConfiguration['workers'], worker, (workerInterface.readyQueue, workerInterface.completedQueue, workerInterface.activeJobs))
     workerInterface.run()
+    pool.close()
+
     logInterface.logging.info('Finished script/scramble.py to scramble tables')
